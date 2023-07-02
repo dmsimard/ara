3ab8bbdd302ec80bc7514f3c8cd15af908fad13d
David Moreau Simard <moi@dmsimard.com>
2023-07-01 22:06:12 -0400

    WIP: UI refresh and update to bootstrap 5
    This is a work in progress that lays much of the groundwork for the
    refresh and update to bootstrap 5.
    
    - Remove bootstrap-darkly and bootstrap-flatly from bootswatch
      We have been using these bootstrap themes in lieu of light and dark
    themes but bootstrap 5.3 has a built-in light/dark theme now.
    
    - Remove jquery, no longer necessary with bootstrap 5
    
    - Cleaned up and split out big chunks of django templates into their
      own partials for improved readability.
    
    Notable Bootstrap 5 resolved issues:
    
    - Fixed the light/dark mode toggle and made it use the new native
      bootstrap color modes
    - Fixed and themed the playbook search accordion
    
    TODO:
    - Fix modals (such as CLI options)
    - Refresh hosts page (currently broken)
    - Refresh tasks page (currently broken)
    - Refresh playbook report page
    - Refresh host report/facts page
    - Refresh task result detail page

---
+++ ara/ui/static/js/theme.js
@@ -0,0 +1,52 @@
+/* Inspired by https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript */
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2023 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+const getStoredTheme = () => localStorage.getItem('theme')
+const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+const getPreferredTheme = () => {
+  const storedTheme = getStoredTheme()
+  if (storedTheme) {
+    return storedTheme
+  }
+
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const setTheme = theme => {
+  if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+    document.getElementById('dark-light-toggle-btn').setAttribute('checked', 'true')
+  }
+  if (theme === 'light') {
+    document.getElementById('dark-light-toggle-btn').removeAttribute('checked')
+  } else {
+    document.getElementById('dark-light-toggle-btn').setAttribute('checked', 'true')
+  }
+}
+
+setTheme(getPreferredTheme())
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+  const storedTheme = getStoredTheme()
+  if (storedTheme !== 'light' && storedTheme !== 'dark') {
+    setTheme(getPreferredTheme())
+  }
+})
+
+document.getElementById('dark-light-toggle-btn').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+      setTheme('light')
+      setStoredTheme('light')
+    }
+    else {
+      setTheme('dark')
+      setStoredTheme('dark')
+    }
+})
